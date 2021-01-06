9b7e0ad82563dbf889d70bad1809f851fefbac15
NivekUCN <72078122+NivekUCN@users.noreply.github.com>
2021-01-06 17:57:20 -0300

    build(api.php): Add the routes

--- web/routes/api.php
+++ web/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NewsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::Resource('news', NewsController::class);
+
+//TODO: add routes of NwsController
+
+Route::post('/register',[AuthController::class,'register']);
+
+Route::post('/login',[AuthController::class,'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+
+});
