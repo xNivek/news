b8111e06a63a071865c7ae7d8a5a5d0ec2ef6a3b
TakeshiNaito <33036842+TakeshiNaito@users.noreply.github.com>
2021-01-14 01:51:55 -0300

    chore(APP ): add icon and update night mode switch

--- app/src/main/java/cl/ucn/disc/dsm/jramirez/news/activities/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dsm/jramirez/news/activities/MainActivity.java
@@ -68,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
 
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
