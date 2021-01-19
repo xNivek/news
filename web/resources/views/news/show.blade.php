6e312ba0b2b678de7bcd03772b82a7d96a5b1cd7
JotapeValdesCondori <juan.valdes@alumnos.ucn.cl>
2021-01-19 03:05:32 -0300

    refactor(news): create show view

---
+++ web/resources/views/news/show.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+<!--Hereda código de app.blade-->
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-md-offset-2">
+                <div class="card">
+                    <div class="card-header">
+                        Ver trabajo
+                        <a href="" style="float:right" class= "btn btn-primary">Editar</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
